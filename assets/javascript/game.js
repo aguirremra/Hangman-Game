78048b7fcf856b44c0d2f607db6be83c558f1093
= <aguirremra@gmail.com>
2017-08-19 00:18:42 -0700

    my final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,136 @@
-var charCode, keyPress, wins, lettersGuessed, guessesLeft, message, winsCount, guessesCount, letter, wordToGuess, blanks, arr, blankSpaces;
+var keyPress,
+  words,
+  keyCode,
+  lettersGuessed,
+  wins,
+  losses,
+  winsCount,
+  wordToGuess,  
+  placeHolder,
+  guessedWord,
+  guessesRemaining,
+  lettersAlreadyGuessed,
+  audioword,
+  image;
 
-arr = "";
-blanks = "";
+words = ["incubus", "cake", "madonna"];
 
 //get elements
-wins = document.getElementById("wins");
-lettersGuessed = document.getElementById("lettersGuessed");
-randomLetter = document.getElementById("randomLetter");
-guessesLeft = document.getElementById("guessesLeft");
-message = document.getElementById("message");
-blanks = document.getElementById("blanks");
-
+  blanks = document.getElementById("blanks");
+  guessedWord = document.getElementById("guessedWord");
+  wins = document.getElementById("wins");
+  guessesLeft = document.getElementById("guessesLeft");
+  lettersAlreadyGuessed = document.getElementById("lettersAlreadyGuessed");
+  image = document.getElementById("myimage");
 
 resetVariables();
 setVariables();
 
 function setVariables(){
-	wordToGuess = "Disneyland";	
-	keyCode = "";
-	keyPress = "";
-	guessesCount = 12;
-	getlettersGuessed();
-	arr = [];
-	blankSpaces = [];
-	
-	for(i = 0; i < wordToGuess.length; i++){
-		blankSpaces.push("_");
-	}
-	blanks.innerHTML = "Word to Guess   " + blankSpaces.join(" ");
-
-}
-function resetVariables(){	
-	winsCount = 0;
-	getWinsCount();
+  wordToGuess = words[Math.floor(Math.random() * words.length)].toUpperCase();
+  console.log(wordToGuess);
+  keyCode = "";
+  keyPress = "";
+  audioword = "";
+  guessesRemaining = 8;  
+  placeHolder = [];
+  lettersGuessed = [];
+  hideWord();
+  displayWins();
+  displayRemaining();
+  getLettersGuessed();
 }
 
-function getWinsCount(){
-	wins.innerHTML = "Wins: " + winsCount; 
-}
-function getlettersGuessed(){
-	lettersGuessed.innerHTML = "Letters so far: " + arr;
-}
-function getGuessesCount(){
-	guessesLeft.innerHTML = "Guesses Left: " + guessesCount;
-}	
-
-document.onkeyup=function(event){
-    keyPress = event.key;
-    keyCode = event.keyCode;
-    if(validateKeys(keyCode)){
-    	arr.push(keyPress);
-    	getlettersGuessed();
-	    	if(checkWinsLosses(keyPress)){
-		    	getWinsCount();
-		    	setVariables();
-	    	}else{
-		    	guessesCount--;
-		    	getGuessesCount();
-		    	checkGuesses(guessesCount);
-	    	} 	
-    } else{
-    	setVariables();
-    }  
-}
-function checkGuesses(count){
-		if(count === 0){
-			lossesCount++;
-			getLossesCount();
-			getGuessesCount();
-			setVariables();
-		}
+function resetVariables(){
+  winsCount = 0;
 }
 
-function checkWinsLosses(keyPress){
-	var letterIndex = wordToGuess.indexOf(keyPress);
-	console.log(letterIndex);
-	
-	if(letterIndex >= 0){			
-		winsCount++;
-		return true;
-	}else{
-		return false;
-	}
-}
-function validateKeys(keyCode){
-	if(keyCode > 64 && keyCode < 91){
-		return true;
-    } else{
-    	return false;
+//on key press...
+document.onkeyup = function(event) {
+  keyPress = event.key.toUpperCase();
+  keyCode = event.keyCode;
+    //if the key press is valid
+    if (validateKeys(keyCode)) {
+      //if the letter is in the word to guess
+      if (wordToGuess.indexOf(keyPress) != -1) {
+        //replace blank with letter pressed
+        for (var i = 0; i < wordToGuess.length; i++) {
+          if (keyPress === wordToGuess.charAt(i)) {
+            placeHolder[i] = keyPress;
+            displayLetters();
+          }
+        }
+          //has the word been guessed yet?
+          if (placeHolder.join("") === wordToGuess) {
+            displayGuessedWord();
+            audioword = wordToGuess;
+            playAudio(audioword);
+            setImage(audioword);
+            winsCount++;
+            setVariables();
+        }
+
+      }else{
+        if(lettersGuessed.indexOf(keyPress) === -1){
+          if(guessesRemaining === 0){
+            alert("Go ahead and try again.");
+            setVariables();
+            return;
+          }
+          else{
+            guessesRemaining--;
+            displayRemaining();
+            lettersGuessed.push(keyPress);
+            getLettersGuessed();
+          }
+        }
+      }
+      //if the key press is not valid
+    } else {
+      alert("not a valid key");
     }
+  }
+  if(guessesRemaining === 0){
+  alert("Game is over");
+  setVariables();
+  }
+
+function displayGuessedWord(){
+  guessedWord.innerHTML = wordToGuess;
+}
+function getLettersGuessed(){
+  lettersAlreadyGuessed.innerHTML = lettersGuessed;
+}
+
+function displayRemaining(){
+  guessesLeft.innerHTML = guessesRemaining;
+}
+function displayWins() {
+  wins.innerHTML = "Wins: " + winsCount;
+}
+
+function displayLetters() {
+  blanks.innerHTML = placeHolder.join(" ");
+}
+//display placeholders to screen
+function hideWord() {
+  for (i = 0; i < wordToGuess.length; i++) {
+    placeHolder.push("__");
+  }
+  displayLetters();
+}
+
+function validateKeys(keyCode) {
+  if (keyCode > 64 && keyCode < 91) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function playAudio(audioword){ 
+    aud = new Audio("assets/audio/" + audioword + ".mp3");
+        aud.play();
+  }
+function setImage(audioword){
+  image.src = "assets/images/" + audioword + ".jpeg";
 }
